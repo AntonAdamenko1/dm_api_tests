b453f561e30acbee8f93d6a005143d6c0e52115f
Anton <adamav1@ya.ru>
2024-08-17 14:34:35 +0700

    Create fixture

--- test_account.py
+++ test_account.py
@@ -1,20 +1,29 @@
 import requests
 import json
+import pytest
 
-
-def test_account():
-    url = "http://5.63.153.31:5051/v1/account"
-
-    payload = json.dumps({
+@pytest.fixture()
+def generate_user():
+    return {
       "login": "login123456791",
       "email": "login1234567891@mail.ru",
       "password": "login1234567891"
-    })
-    headers = {
+    }
+@pytest.fixture()
+def set_url():
+    return "http://5.63.153.31:5051/v1/account"
+
+@pytest.fixture()
+def headers():
+    return {
       'accept': '*/*',
       'Content-Type': 'application/json'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+def test_post_v1_account(set_url, headers, generate_user):
+    response = requests.request("POST", set_url, headers=headers, json=generate_user)
+    print(response.text)
 
+def test_post_v1_account2(set_url, headers, generate_user):
+    response = requests.request("POST", set_url, headers=headers, json=generate_user)
     print(response.text)
