2eeb6eda67e77d7b1ca5e23854ddfa039a2e25c6
Anton <adamav1@ya.ru>
2024-08-21 23:28:15 +0700

    Create mark parametrize

--- test_account.py
+++ test_account.py
@@ -22,7 +22,34 @@ def headers():
       'Content-Type': 'application/json'
     }
 
+data=[
+    # короткий логин
+    {
+        "login": "l",
+        "email": "email_12345@mail.ru",
+        "password": "12345678"
+    },
+    # невалидный емайл
+    {
+        "login": "login_12345678902",
+        "email": "e",
+        "password": "12345678"
+    },
+    # короткий пароль
+    {
+        "login": "login_12345678902",
+        "email": "email_12345@mail.ru",
+        "password": "1"
+    }
+]
+
 def test_post_v1_account(set_url, headers, generate_user):
     print(generate_user)
     response = requests.request("POST", set_url, headers=headers, json=generate_user)
+    print(response.text)
+
+@pytest.mark.parametrize('data0',data)
+def test_post_v1_account_negative(set_url,headers,data0):
+    print(data)
+    response=requests.request("POST",set_url,headers=headers,json=data0)
     print(response.text)
